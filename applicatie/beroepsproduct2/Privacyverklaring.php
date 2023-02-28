9d5ccfe0d341e510e0d2a59ab4a90906c4744a50
Bramdejonggg <djbramovic@outlook.com>
2023-02-28 21:36:03 +0100

    Create Privacyverklaring.php

---
+++ applicatie/beroepsproduct2/Privacyverklaring.php
@@ -0,0 +1,108 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="nl">
+
+<!-- Header -->
+
+<head>
+    <meta charset="utf-8">
+    <title>Gelre Airport</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+
+<!-- Body -->
+
+<body>
+
+    <!-- Navigatiebalk -->
+
+    <nav>
+        <img src="image/Logo.jpg.png" alt="niks">
+        <ul>
+            <li><a href="Index.php">Home</a></li>
+            <li><a href="vluchten.php">Vluchten</a></li>
+            <li><a href="Privacyverklaring.php">Privacyverklaring</a></li>
+        </ul>
+    </nav>
+    <div class="Tekst">
+        <h1>Privacyverklaring</h1>
+        <h2>Privacyverklaring Gelre Airport<br>
+            februari 2022</h2><br>
+        Uw privacy is voor Gelre Airport van groot belang. Wij houden ons dan ook aan de relevante wet- en regelgeving
+        over privacy, waaronder<br>
+        de Algemene Verordening Gegevensbescherming Dit betekent dat wij:<br>
+        <strong> doeleinden duidelijk vastleggen</strong> voordat wij uw persoonlijke gegevens verwerken, via deze
+        privacyverklaring;<br>
+        <strong>Zo min mogelijk persoonlijke gegevens opslaan</strong> en enkel de gegevens die nodig zijn voor onze
+        doeleinden.<br>
+        <strong>Expliciet toestemming vragen</strong> voor de verwerking van uw persoonlijke gegevens, mocht toestemming
+        verplicht zijn.<br>
+        <strong>Benodigde beveiligingsmaatregelen treffen</strong> om uw persoonlijke gegevens te beschermen. Wij leggen
+        deze verplichtingen ook<br>
+        op aan partijen die persoonsgegevens voor ons verwerken.<br>
+        <strong>Uw rechten respecteren</strong>, zoals het recht op inzage, correctie of verwijdering van uw bij ons
+        verwerkte persoonsgegevens.<br>
+        Uw gegevens zijn veilig bij ons en wij zullen deze gegevens altijd netjes gebruiken. In deze privacyverklaring
+        leggen we uit wat we bij<br>
+        de website Gelre Airport allemaal doen met informatie die we over u te weten komen.<br>
+        Als u vragen heeft of wilt weten wat we precies van u bijhouden, neem dan contact op met Gelre Airport.<br>
+        <h3>Afhandelen bestelling</h3><br>
+        Wanneer u bij ons een bestelling plaatst, gebruiken wij uw persoonsgegevens om deze netjes af te kunnen
+        handelen.<br> Wij mogen uw
+        persoonsgegevens dan aan onze bezorgdienst geven om de bestelling bij u te laten bezorgen. Ook krijgen wij
+        informatie over uw
+        betaling van uw bank of creditcardmaatschappij.<br>
+        Hiervoor gebruiken wij uw naw-gegevens, telefoonnummer, e-mailadres, betalingsgegevens en geboortedatum. Dit
+        doen wij op basis<br>
+        van uw toestemming. Wij bewaren deze informatie tot uw bestelling is afgerond. Bepaalde klantgegevens
+        bewaren wij langer in<br>
+        verband met de wettelijke fiscale bewaarplicht.<br>
+        Verstrekken aan derden<br>
+        Wij werken met bepaalde bedrijven samen, die uw hierboven genoemde persoonsgegevens van ons kunnen
+        ontvangen.<br>
+        van nieuwsbrieven<br>
+        Wij hebben een nieuwsbrief en u wordt automatisch toegevoegd aan de lijst van abonnees. In de nieuwsbrief
+        leest u nieuwtjes, tips<br>
+        en informatie over onze producten en diensten.<br>
+        Hiervoor gebruiken wij uw naw-gegevens en e-mailadres. Dit doen wij op basis van uw toestemming. Wij bewaren
+        deze informatie<br>
+        totdat u het abonnement opzegt.<br>
+        Het abonnement op de nieuwsbrief kunt u op ieder moment opzeggen. Iedere nieuwsbrief bevat een
+        afmeldlink.<br>
+        Verstrekken aan derden<br>
+        Wij werken met bepaalde bedrijven samen, die uw hierboven genoemde persoonsgegevens van ons kunnen
+        ontvangen.<br>
+        <h3>Reclame</h3><br>
+        Wij willen u graag reclame sturen over aanbiedingen en nieuwe producten of diensten. Dit doen wij:<br>
+        per e-mail<br>
+        U kunt op ieder moment bezwaar maken tegen deze reclame. Elke e-mail bevat een afmeldlink.<br>
+        <h3>Advertenties</h3><br>
+        Onze website vertoont algemene advertenties. Hierbij houden we geen persoonsgegevens bij, we komen dus ook
+        niet te weten of u<br>
+        deze leuk vindt.<br>
+        Verstrekking aan andere bedrijven of instellingen<br>
+        Met uitzondering van de hierboven genoemde partijen, geven wij uw persoonsgegevens onder geen voorwaarde aan
+        andere<br>
+        bedrijven of instellingen, behalve als wij dat wettelijk verplicht zijn (bijvoorbeeld als de politie dat
+        eist bij een vermoeden van een<br>
+        misdrijf).<br>
+        <h3>Statistieken</h3><br>
+        Wij houden statistieken bij over het gebruik van onze website, maar deze gegevens zijn altijd
+        geanonimiseerd.<br>
+        In onze website zijn social media buttons opgenomen. Hiermee verzamelen de beheerders van deze diensten uw
+        persoonsgegevens.<br>
+    </div>
+
+    <!-- Footer -->
+
+    <footer>
+        @Copyright GelreAirport 2022- All Right Reserved.
+    </footer>
+</body>
+
+</html>
