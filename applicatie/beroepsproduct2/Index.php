06044c47854908f1699e1814b60955ac2fbf9cc9
Bramdejonggg <djbramovic@outlook.com>
2023-02-28 21:35:49 +0100

    Create Index.php

---
+++ applicatie/beroepsproduct2/Index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+
+<!-- Header -->
+
+<head>
+    <meta charset="utf-8">
+    <title>Gelre Airport</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+
+<!-- Body -->
+
+<body>
+
+    <!-- Navigatiebalk -->
+
+    <nav>
+        <img src="image/Logo.jpg.png" alt="niks">
+        <ul>
+            <li><a href="Index.php">Home</a></li>
+            <li><a href="vluchten.php">Vluchten</a></li>
+            <li><a href="Privacyverklaring.php">Privacyverklaring</a></li>
+        </ul>
+    </nav>
+    <div class="login">
+        <h1>Welkom bij gelre airport</h1>
+        <br>
+        <br>
+        <a class="button" href="login.php">LOGIN</a>
+    </div>
+
+      <!-- Footer -->
+
+    <footer>
+        @Copyright GelreAirport 2022- All Right Reserved.
+    </footer>
+</body>
+
+</html>
